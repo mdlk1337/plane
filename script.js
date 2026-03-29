8372c6d562bbcdfd309de766b38beb0c30e522f5
mdlk1337 <mdlk.mdlk@mail.ru>
2026-03-30 01:01:42 +0300

    end

---
+++ script.js
@@ -0,0 +1,55 @@
+ const plane = document.querySelector(".plane");
+ const box = document.querySelector(".box")
+
+let stepPlane = 0;
+let widthWindow = window.innerWidth;
+let isRight = true 
+let boxY = 150;
+
+function startPlane() {
+     setInterval(() => {
+        if(isRight) {
+            stepPlane++;
+
+            plane.style.left = stepPlane + 'px';
+
+            if(stepPlane + 300  === widthWindow/2) {
+                box.style.display = "block";
+                dropBox()
+            }
+
+            if(stepPlane + 300 === widthWindow) {
+                isRight = false;
+                plane.style.transform = "scaleX(1)";
+            }
+        }else {
+            stepPlane--;
+
+            plane.style.left = stepPlane + 'px';
+            
+            if(stepPlane === 0) {
+                isRight = true
+                plane.style.transform = "scaleX(-1)";
+                box.style.display = 'none'
+            }
+        }
+    }, 1)
+}
+
+ function dropBox() {
+    
+     let a = setInterval(() => {
+        boxY++
+        box.style.top = boxY + 'px'
+        
+        if(boxY === 800) {
+            clearInterval(a)
+            setTimeout(dropBox, 100000);
+            boxY = 150
+        }
+    }, 1)
+}
+
+
+startPlane()
+
